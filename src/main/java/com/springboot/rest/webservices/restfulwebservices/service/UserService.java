f5e49b44921cf3b50f80444554a7829efc10a658
Katerina <kkentroti@gmail.com>
2023-02-23 20:00:11 +0200

    service layer added

---
+++ src/main/java/com/springboot/rest/webservices/restfulwebservices/service/UserService.java
@@ -0,0 +1,57 @@
+package com.springboot.rest.webservices.restfulwebservices.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.springboot.rest.webservices.restfulwebservices.model.User;
+import com.springboot.rest.webservices.restfulwebservices.model.UserNotFoundException;
+import com.springboot.rest.webservices.restfulwebservices.repository.UserRepository;
+
+//Would it be beneficial to make the UserService an interface and provide implementation(s) for the Interface?
+@Service
+public class UserService {
+	
+	UserRepository userRepository;
+	
+	public UserService(UserRepository userepository) {  //constructor-based injection
+		
+		this.userRepository=userepository;
+			
+	}
+	
+	public User saveUser(User user) {      //save a new user
+		
+		return userRepository.save(user);     //Should the validation of the username take place at the Service layer or at the controller?
+				
+	}
+	
+	public List<User> getAllUsers() {     //get all users
+		
+		return userRepository.findAll();
+	}
+	
+	
+	public Optional<User> getUserById(int id) {    //return the user, if the user exists
+		
+		Optional<User> user=userRepository.findById(id);
+		
+		if (user.isEmpty())
+			throw new UserNotFoundException("id:"+id);
+	
+	   return user;	
+    }
+	
+	
+	public void deleteUser(int id) {
+		
+		userRepository.deleteById(id);
+	}
+	
+	                                                   
+
+	
+	
+
+}
