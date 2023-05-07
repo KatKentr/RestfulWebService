d819eec854f65ded385720c9eef876dd2c1b1ca1
Katerina <kkentroti@gmail.com>
2023-05-07 12:17:37 +0300

    roleRepository class

---
+++ src/main/java/com/springboot/rest/webservices/socialmediaapp/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.springboot.rest.webservices.socialmediaapp.repository;
+
+import com.springboot.rest.webservices.socialmediaapp.model.Comment;
+import com.springboot.rest.webservices.socialmediaapp.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+	
+	Optional<Role> findByRoleType(String roleType);
+
+}
