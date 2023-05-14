53ed09a3cf8242a174c163454742710d54803f25
Katerina <kkentroti@gmail.com>
2023-05-14 19:49:57 +0300

    switch to jwt authentication-classes to generate jwt token and insert filter in spring security filter chain to check the authorization header of each request

---
+++ src/main/java/com/springboot/rest/webservices/socialmediaapp/jwt/JwtTokenFilter.java
@@ -0,0 +1,101 @@
+package com.springboot.rest.webservices.socialmediaapp.jwt;
+
+import com.springboot.rest.webservices.socialmediaapp.model.Role;
+import com.springboot.rest.webservices.socialmediaapp.model.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.Claims;
+import java.io.IOException;
+
+//To access the secure REST APIs, the client must include an access token in the Authorization header of the request. So we need to insert our own filter in the middle of Spring Security filters chain, before the UsernameAndPasswordAuthenticationFilter, in order to check the Authorization header of each request.
+
+@Component
+public class JwtTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtTokenUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        if (!hasAuthorizationBearer(request)) {   //If the Authorization header of the request doesn’t contain a Bearer token, it continues the filter chain without updating authentication context.
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = getAccessToken(request);
+
+        if (!jwtUtil.validateAccessToken(token)) {
+            filterChain.doFilter(request, response);   //Else, if the token is not verified, continue the filter chain without updating authentication context.
+            return;
+        }
+
+        setAuthenticationContext(token, request);  //If the token is verified, update the authentication context with the user details ID and email. In other words, it tells Spring that the user is authenticated, and continue the downstream filters.
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean hasAuthorizationBearer(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (ObjectUtils.isEmpty(header) || !header.startsWith("Bearer")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private String getAccessToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        String token = header.split(" ")[1].trim();
+        return token;
+    }
+
+    private void setAuthenticationContext(String token, HttpServletRequest request) {
+        UserDetails userDetails = getUserDetails(token);
+
+        UsernamePasswordAuthenticationToken
+                authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        authentication.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private UserDetails getUserDetails(String token) {
+        User userDetails = new User();
+        Claims claims = jwtUtil.parseClaims(token);
+        String subject = (String) claims.get(Claims.SUBJECT);
+        String roles = (String) claims.get("roles");
+
+        System.out.println("SUBJECT: " + subject);
+        System.out.println("roles: " + roles);
+        roles = roles.replace("[", "").replace("]", "");
+        String[] roleNames = roles.split(",");
+
+
+        for (String aRoleName : roleNames) {
+            System.out.println(aRoleName);
+
+            userDetails.addRole(new Role(aRoleName));
+        }
+
+        String[] jwtSubject = subject.split(",");
+
+        userDetails.setId(Integer.parseInt(jwtSubject[0]));
+        userDetails.setEmail(jwtSubject[1]);
+
+        return userDetails;
+    }
+}
