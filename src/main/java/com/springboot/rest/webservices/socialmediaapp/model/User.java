67846031adb5a5ef57754872d6f985a0ef0c12fd
Katerina <kkentroti@gmail.com>
2023-05-18 20:45:42 +0300

    returns from the overriden methods of UserDetails set to JsonIgnore

--- src/main/java/com/springboot/rest/webservices/socialmediaapp/model/User.java
+++ src/main/java/com/springboot/rest/webservices/socialmediaapp/model/User.java
@@ -123,6 +123,7 @@ public class User implements UserDetails {
 
 
 	@Override
+	@JsonIgnore
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
 		for (Role role : roles) {
@@ -146,21 +147,25 @@ public class User implements UserDetails {
 	}
 
 	@Override
+	@JsonIgnore
 	public boolean isAccountNonExpired() {
 		return true;
 	}
 
 	@Override
+	@JsonIgnore
 	public boolean isAccountNonLocked() {
 		return true;
 	}
 
 	@Override
+	@JsonIgnore
 	public boolean isCredentialsNonExpired() {
 		return true;
 	}
 
 	@Override
+	@JsonIgnore
 	public boolean isEnabled() {
 		return true;
 	}
