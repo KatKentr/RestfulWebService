1797d0ef9844df35b59ee566bd743b9f5ec1ded3
Katerina <kkentroti@gmail.com>
2023-06-18 15:28:39 +0300

    methods related to the followers feature

--- src/main/java/com/springboot/rest/webservices/socialmediaapp/service/UserService.java
+++ src/main/java/com/springboot/rest/webservices/socialmediaapp/service/UserService.java
@@ -2,6 +2,7 @@ package com.springboot.rest.webservices.socialmediaapp.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
@@ -14,10 +15,13 @@ import com.springboot.rest.webservices.socialmediaapp.repository.UserRepository;
 public class UserService {
 	
 	UserRepository userRepository;
+
+	AuthService authService;
 	
-	public UserService(UserRepository userRepository) {  //constructor-based injection
+	public UserService(UserRepository userRepository, AuthService authService) {  //constructor-based injection
 		
 		this.userRepository=userRepository;
+		this.authService=authService;
 			
 	}
 	
@@ -48,10 +52,52 @@ public class UserService {
 		
 		userRepository.deleteById(id);
 	}
-	
-	                                                   
 
-	
-	
 
-}
+	public User follow(String usernameToFollow) throws UserNotFoundException {
+		User user=authService.getCurrentUser();
+		Optional<User> toFollowOptional=userRepository.findByUsername(usernameToFollow);  //else throw userNotFoundException
+		User toFollow=toFollowOptional.orElseThrow(() -> new UserNotFoundException("username: "+usernameToFollow));
+		user.addFollowing(toFollow);
+		if (user.getFollowing().contains(toFollow)){
+
+			return toFollow;
+		} else {
+
+			throw new RuntimeException();   //TODO: add relevant message somethig unexpected happen
+		}
+
+	}
+
+	public void unfollow(Integer toUnfollowId) throws UserNotFoundException {
+
+		User user=authService.getCurrentUser();
+		Optional<User> toUnfollowOptional=userRepository.findById(toUnfollowId);  //else throw userNotFoundException
+		User toUnfollow=toUnfollowOptional.orElseThrow(() -> new UserNotFoundException("id: "+toUnfollowId));
+		if (user.getFollowing().contains(toUnfollow)) {
+			user.removeFollowing(toUnfollow);
+		}
+
+	}
+
+	public Set<User> getFollowersOfUser(Integer userId) throws UserNotFoundException {     //TODO: replace return type ith UserDto
+
+		Optional<User> userOptional = userRepository.findById(userId);
+		User user = userOptional.orElseThrow(() -> new UserNotFoundException("id: " + userId));
+
+		return user.getFollowers();
+
+	}
+
+	public Set<User> getFollowingUsers(Integer userId) throws UserNotFoundException {
+
+		Optional<User> userOptional = userRepository.findById(userId);
+		User user = userOptional.orElseThrow(() -> new UserNotFoundException("id: " + userId));
+		return user.getFollowing();
+
+	}
+
+
+
+
+	}
